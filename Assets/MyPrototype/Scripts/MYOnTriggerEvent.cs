3ad65af57e85a8228e4b8584a003c60a279c0ece
정찬호 <54787512+jch1999@users.noreply.github.com>
2023-04-24 23:08:34 +0900

    Start creating muy onTriggerEvent scripts

---
+++ Assets/MyPrototype/Scripts/MYOnTriggerEvent.cs
@@ -0,0 +1,42 @@
+using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(Collider))]
+public class MYOnTriggerEvent : MonoBehaviour
+{
+    public LayerMask targetLayer;
+    //for my prototype,there need to be emission  or emission outline effects for item when player enter the item's detection bound.
+    [Header("Trigger Enter Event Section")]
+    public UnityEvent onTriggerEnterEvent;
+    
+    //Item's effects should be turned off, when player exit from it's detection bound.
+    [Space(10)]
+    [Header("Trigger Exit Event Section")]
+    public UnityEvent onTriggerExitEvent;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.layer!=targetLayer)
+            return;
+    }
+    
+    void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject.layer!=targetLayer)
+            return;
+        
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
